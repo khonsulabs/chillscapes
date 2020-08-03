ac420cc2e2d46dc4d2d00e734e7a62d2f208044f
Jonathan Johnson <jon@khonsulabs.com>
2020-08-02 19:54:37 -0700

    Added dev assets and a click track. Ugly code.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use kludgine::prelude::*;
+mod game;
+use game::Game;
+
 fn main() {
-    println!("Hello, world!");
+    SingleWindowApplication::run(Chillscapes::default());
+}
+
+#[derive(Default)]
+struct Chillscapes {
+    game: Entity<Game>,
+}
+impl Window for Chillscapes {}
+
+impl WindowCreator<Chillscapes> for Chillscapes {
+    fn window_title() -> String {
+        "Chillscapes".to_owned()
+    }
+}
+impl StandaloneComponent for Chillscapes {}
+
+#[async_trait]
+impl Component for Chillscapes {
+    async fn initialize(&mut self, context: &mut SceneContext) -> KludgineResult<()> {
+        self.game = self.new_entity(context, Game::default()).insert().await?;
+        Ok(())
+    }
+
+    async fn layout(
+        &mut self,
+        _context: &mut StyledContext,
+    ) -> KludgineResult<Box<dyn LayoutSolver>> {
+        Layout::absolute()
+            .child(
+                self.game,
+                AbsoluteBounds::from(Surround::uniform(Dimension::Points(0.))),
+            )?
+            .layout()
+    }
 }
