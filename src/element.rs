b5f5b80694b61725eec998b05dfe29bf62de6f2b
Jonathan Johnson <jon@khonsulabs.com>
2020-08-06 17:29:13 -0700

    Made clicking elements "lock" it in.

--- src/element.rs
+++ src/element.rs
@@ -2,6 +2,7 @@ use crate::beats_per_second;
 use kludgine::prelude::*;
 use rodio::{decoder::Decoder, source::Buffered};
 use std::{
+    collections::VecDeque,
     io::Cursor,
     time::{Duration, Instant},
 };
@@ -9,6 +10,11 @@ use std::{
 #[derive(Debug, Clone)]
 pub enum ElementCommand {
     SetBeat { beat: f32, measure: usize },
+}
+
+#[derive(Debug, Clone)]
+pub enum ElementMessage {
+    ImageEvent(ControlEvent),
 }
 
 #[derive(Debug, Clone)]
@@ -24,6 +30,8 @@ pub struct Element {
     star: Entity<Image>,
     measure: usize,
     current_beat: Option<usize>,
+    beats_to_hit: VecDeque<Instant>,
+    progress: f32,
     alpha_animator: RequiresInitialization<AnimationManager<ImageAlphaAnimation>>,
     frame_animator: RequiresInitialization<AnimationManager<ImageFrameAnimation>>,
 }
@@ -41,7 +49,9 @@ impl Element {
             click,
             beats,
             measure: 0,
+            progress: 0.,
             current_beat: None,
+            beats_to_hit: VecDeque::default(),
             star: Entity::default(),
             alpha_animator: Default::default(),
             frame_animator: Default::default(),
@@ -63,6 +73,35 @@ impl Element {
 
         (beat_start, current_beat)
     }
+
+    async fn increment_progress(&mut self, context: &mut Context, factor: f32) {
+        // Add to progress so that 2 measures of perfect hits = 1.0
+        self.progress += 1. / (self.beats.len() as f32 * 2.) * factor;
+
+        println!(
+            "Something happened {}, new progress: {}",
+            factor, self.progress
+        );
+        if self.progress < 0. {
+            self.progress = 0.;
+        } else if self.progress >= 1. {
+            self.progress = 1.;
+            self.callback(context, ElementEvent::LoopLockedIn).await;
+        }
+    }
+
+    async fn deduct_missed_beats(&mut self, context: &mut Context) {
+        let now = Instant::now();
+
+        if let Some(beat_instant) = self.beats_to_hit.front() {
+            let delta = instant_delta_in_millis(*beat_instant, now);
+
+            if delta < -50 {
+                self.increment_progress(context, -1.).await;
+                self.beats_to_hit.pop_front();
+            }
+        }
+    }
 }
 
 #[async_trait]
@@ -71,6 +110,7 @@ impl Component for Element {
         let sprite = include_aseprite_sprite!("../assets/ecton/star").await?;
         self.star = self
             .new_entity(context, Image::new(sprite))
+            .callback(ElementMessage::ImageEvent)
             .insert()
             .await?;
 
@@ -87,10 +127,19 @@ impl Component for Element {
         Ok(())
     }
 
-    async fn update(&mut self, _context: &mut SceneContext) -> KludgineResult<()> {
+    async fn update(&mut self, context: &mut SceneContext) -> KludgineResult<()> {
+        self.deduct_missed_beats(context).await;
         self.alpha_animator.update().await;
         self.frame_animator.update().await;
         Ok(())
+    }
+}
+
+fn instant_delta_in_millis(a: Instant, b: Instant) -> i128 {
+    if let Some(delta) = a.checked_duration_since(b) {
+        delta.as_millis() as i128
+    } else {
+        -(b.checked_duration_since(a).unwrap().as_millis() as i128)
     }
 }
 
@@ -98,7 +147,7 @@ const INITIAL_ALPHA: f32 = 0.1;
 
 #[async_trait]
 impl InteractiveComponent for Element {
-    type Message = ();
+    type Message = ElementMessage;
     type Input = ElementCommand;
     type Output = ElementEvent;
 
@@ -137,6 +186,8 @@ impl InteractiveComponent for Element {
                             .checked_add(Duration::from_secs_f32(remaining_seconds))
                             .unwrap();
 
+                        self.beats_to_hit.push_back(next_beat_instant);
+
                         // Start at 10 ms behind when the beat will hit, so that the fade-in happens over 10ms and it
                         // peaks on the beat
                         let next_beat_start = next_beat_instant
@@ -149,7 +200,9 @@ impl InteractiveComponent for Element {
 
                         // Fade into the target alpha
                         self.alpha_animator.push_frame(
-                            self.star.animate().alpha(1.0, LinearTransition),
+                            self.star
+                                .animate()
+                                .alpha(self.progress * 0.7 + 0.3, LinearTransition),
                             next_beat_instant,
                         );
 
@@ -190,6 +243,39 @@ impl InteractiveComponent for Element {
                                 .frame(Some("Idle"), 0., LinearTransition),
                             frame_end.checked_add(Duration::from_millis(1)).unwrap(),
                         );
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+
+    async fn receive_message(
+        &mut self,
+        context: &mut Context,
+        message: Self::Message,
+    ) -> KludgineResult<()> {
+        match message {
+            ElementMessage::ImageEvent(ControlEvent::Clicked(_)) => {
+                let now = Instant::now();
+
+                if let Some(beat_instant) = self.beats_to_hit.pop_front() {
+                    let delta = instant_delta_in_millis(beat_instant, now);
+                    println!("Click delta: {}", delta);
+                    match delta {
+                        i128::MIN..=-51 | 51..=150 => {
+                            // Missed the beat entirely or clicked a bit too soon
+                            self.increment_progress(context, -1.).await;
+                        }
+                        -50..=50 => {
+                            self.increment_progress(context, 1.).await;
+                        }
+                        151..=i128::MAX => {
+                            // Far in the future, the click should count against the player
+                            // but the beat should still be clickable.
+                            self.increment_progress(context, -1.).await;
+                            self.beats_to_hit.push_front(beat_instant);
+                        }
                     }
                 }
             }
